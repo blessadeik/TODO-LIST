6b88e2824a38ad58a1480c810e3e5b05ff33d92f
ADEIKA, Blessing Isoyiza <blessadeik@gmail.com>
2022-05-24 17:14:23 +0100

    JavaScript added

--- todolist.js
+++ todolist.js
@@ -1,25 +1,12 @@
-// JavaScript for TodoList
-
-document.addEventListener('DOMContentLoaded', () => {
-
-function newTasks(){
-    var li = document.createElement('li');
-    var inputValue = document.getElementById('inputTask').value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputTask === ''){
-        alert("Input a Task");
-    }else{
-        document.getElementById('myUl').appendChild(li);
-    }
-
-    document.getElementById("inputTask").value = " ";
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className ="close";
-    span.appendChild(txt);
-    li.appendChild(span);
+function newTask() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("inputTask").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Input a Task");
+  } else {
+    document.getElementById("myUl").appendChild(li);
+  }
+  document.getElementById("inputTask").value = "";
 }
-
-
-})
